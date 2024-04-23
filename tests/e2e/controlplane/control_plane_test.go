a6d912285dd2feca11e4383348d37c3027b45e9e
frherrer <frherrer@redhat.com>
2024-04-23 08:16:43 +0000

    Update SetDefaultEventuallyTimeout for e2e test
    Signed-off-by: frherrer <frherrer@redhat.com>

--- tests/e2e/controlplane/control_plane_test.go
+++ tests/e2e/controlplane/control_plane_test.go
@@ -43,7 +43,7 @@ import (
 var istiodVersionRegex = regexp.MustCompile(`Version:"(\d+\.\d+(\.\d+|-\w+))`)
 
 var _ = Describe("Control Plane Installation", Ordered, func() {
-	SetDefaultEventuallyTimeout(60 * time.Second)
+	SetDefaultEventuallyTimeout(120 * time.Second)
 	SetDefaultEventuallyPollingInterval(time.Second)
 
 	debugInfoLogged := false
