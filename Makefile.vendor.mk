f0dad80a65364a7e541a0c1b66c416a70f7c30aa
Filip Brychta <fbrychta@redhat.com>
2024-04-25 11:50:25 +0200

    Using values for TP.1 version (#13)
    Signed-off-by: Daniel Grimm <dgrimm@redhat.com>

--- Makefile.vendor.mk
+++ Makefile.vendor.mk
@@ -1,6 +1,6 @@
-VERSION = 3.0.0
+VERSION = 3.0.0-tp.1
 OPERATOR_NAME = servicemeshoperator3
 HUB = quay.io/maistra-dev
-CHANNELS = 3.0
+CHANNELS = candidates
 HELM_VALUES_FILE = ossm/values.yaml
 VERSIONS_YAML_FILE = versions.yaml
