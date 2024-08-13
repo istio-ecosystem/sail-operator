a578ce1a31b54d0294a95a2ec46ad1b7059bbcd1
Daniel Grimm <dgrimm@redhat.com>
2024-08-13 11:23:50 +0200

    Update deps & Istio Signed-off-by: Daniel Grimm <dgrimm@redhat.com>

--- api/v1alpha1/istiocni_types.go
+++ api/v1alpha1/istiocni_types.go
@@ -28,10 +28,10 @@ const (
 type IstioCNISpec struct {
 	// +sail:version
 	// Defines the version of Istio to install.
-	// Must be one of: v1.22.1, v1.22.0, v1.21.3, v1.21.2, v1.21.0, latest.
-	// +operator-sdk:csv:customresourcedefinitions:type=spec,order=1,displayName="Istio Version",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:fieldGroup:General", "urn:alm:descriptor:com.tectonic.ui:select:v1.22.1", "urn:alm:descriptor:com.tectonic.ui:select:v1.22.0", "urn:alm:descriptor:com.tectonic.ui:select:v1.21.3", "urn:alm:descriptor:com.tectonic.ui:select:v1.21.2", "urn:alm:descriptor:com.tectonic.ui:select:v1.21.0", "urn:alm:descriptor:com.tectonic.ui:select:latest"}
-	// +kubebuilder:validation:Enum=v1.22.1;v1.22.0;v1.21.3;v1.21.2;v1.21.0;latest
-	// +kubebuilder:default=v1.22.1
+	// Must be one of: v1.22.3, v1.22.1, v1.22.0, v1.21.5, v1.21.3, v1.21.2, v1.21.0, latest.
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,order=1,displayName="Istio Version",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:fieldGroup:General", "urn:alm:descriptor:com.tectonic.ui:select:v1.22.3", "urn:alm:descriptor:com.tectonic.ui:select:v1.22.1", "urn:alm:descriptor:com.tectonic.ui:select:v1.22.0", "urn:alm:descriptor:com.tectonic.ui:select:v1.21.5", "urn:alm:descriptor:com.tectonic.ui:select:v1.21.3", "urn:alm:descriptor:com.tectonic.ui:select:v1.21.2", "urn:alm:descriptor:com.tectonic.ui:select:v1.21.0", "urn:alm:descriptor:com.tectonic.ui:select:latest"}
+	// +kubebuilder:validation:Enum=v1.22.3;v1.22.1;v1.22.0;v1.21.5;v1.21.3;v1.21.2;v1.21.0;latest
+	// +kubebuilder:default=v1.22.3
 	Version string `json:"version"`
 
 	// +sail:profile
@@ -177,7 +177,7 @@ type IstioCNI struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	// +kubebuilder:default={version: "v1.22.1", namespace: "istio-cni"}
+	// +kubebuilder:default={version: "v1.22.3", namespace: "istio-cni"}
 	Spec IstioCNISpec `json:"spec,omitempty"`
 
 	Status IstioCNIStatus `json:"status,omitempty"`
