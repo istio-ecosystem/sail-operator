b0582d529d44d71ea84ada9524cb18896096fcf1
bmangoen <bmangoen@redhat.com>
2024-09-10 17:40:46 +0200

    Use github.actor vars
    Signed-off-by: bmangoen <bmangoen@redhat.com>

--- hack/operatorhub/publish-bundle.sh
+++ hack/operatorhub/publish-bundle.sh
@@ -23,9 +23,8 @@ source "${CUR_DIR}"/../validate_semver.sh
 GITHUB_TOKEN="${GITHUB_TOKEN:-}"
 GIT_USER="${GIT_USER:-}"
 
-# By default, using the last commiter's git config user.name and user.email
-GIT_CONFIG_USER_NAME="${GIT_CONFIG_USERNAME:-$(git log -n 1 --pretty=format:%an)}"
-GIT_CONFIG_USER_EMAIL="${GIT_CONFIG_USER_EMAIL:-$(git log -n 1 --pretty=format:%ae)}"
+GIT_CONFIG_USER_NAME="${GIT_CONFIG_USER_NAME:-}"
+GIT_CONFIG_USER_EMAIL="${GIT_CONFIG_USER_EMAIL:-}"
 
 # The OPERATOR_NAME is defined in Makefile
 : "${OPERATOR_NAME:?"Missing OPERATOR_NAME variable"}"
@@ -100,8 +99,13 @@ BUNDLE_DIR="${CUR_DIR}"/../../bundle
 mkdir -p "${OPERATORS_DIR}"
 cp -a "${BUNDLE_DIR}"/. "${OPERATORS_DIR}"
 
-skipInDryRun git config user.name "${GIT_CONFIG_USER_NAME}"
-skipInDryRun git config user.email "${GIT_CONFIG_USER_EMAIL}"
+if ! git config --global user.name; then
+  skipInDryRun git config --global user.name "${GIT_CONFIG_USER_NAME}"
+fi
+
+if ! git config --global user.email; then
+  skipInDryRun git config --global user.email "${GIT_CONFIG_USER_EMAIL}"
+fi
 
 TITLE="operator ${OPERATOR_NAME} (${OPERATOR_VERSION})"
 skipInDryRun git add .
