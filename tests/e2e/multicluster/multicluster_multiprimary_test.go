41f343bac4571aeee5e4f3ddbd5d9f672ee9c9d1
Marko Lukša <mluksa@redhat.com>
2024-10-10 13:39:31 +0200

    Optimize multicluster e2e tests
    - Install operators on both clusters concurrently
    - Perform namespace deletions in parallel
    
    Signed-off-by: Marko Lukša <mluksa@redhat.com>

--- tests/e2e/multicluster/multicluster_multiprimary_test.go
+++ tests/e2e/multicluster/multicluster_multiprimary_test.go
@@ -53,13 +53,13 @@ var _ = Describe("Multicluster deployment models", Ordered, func() {
 			Expect(helm.Install("sail-operator", filepath.Join(project.RootDir, "chart"), "--namespace "+namespace, "--set=image="+image, "--kubeconfig "+kubeconfig)).
 				To(Succeed(), "Operator failed to be deployed in Cluster #1")
 
+			Expect(helm.Install("sail-operator", filepath.Join(project.RootDir, "chart"), "--namespace "+namespace, "--set=image="+image, "--kubeconfig "+kubeconfig2)).
+				To(Succeed(), "Operator failed to be deployed in Cluster #2")
+
 			Eventually(common.GetObject).
 				WithArguments(ctx, clPrimary, kube.Key(deploymentName, namespace), &appsv1.Deployment{}).
 				Should(HaveCondition(appsv1.DeploymentAvailable, metav1.ConditionTrue), "Error getting Istio CRD")
 			Success("Operator is deployed in the Cluster #1 namespace and Running")
-
-			Expect(helm.Install("sail-operator", filepath.Join(project.RootDir, "chart"), "--namespace "+namespace, "--set=image="+image, "--kubeconfig "+kubeconfig2)).
-				To(Succeed(), "Operator failed to be deployed in Cluster #2")
 
 			Eventually(common.GetObject).
 				WithArguments(ctx, clRemote, kube.Key(deploymentName, namespace), &appsv1.Deployment{}).
@@ -254,20 +254,18 @@ spec:
 				})
 
 				AfterAll(func(ctx SpecContext) {
-					// Delete namespace to ensure clean up for new tests iteration
-					Expect(k1.DeleteNamespace(controlPlaneNamespace)).To(Succeed(), "Namespace failed to be deleted on Cluster #1")
-					Expect(k2.DeleteNamespace(controlPlaneNamespace)).To(Succeed(), "Namespace failed to be deleted on Cluster #2")
+					// Delete namespaces to ensure clean up for new tests iteration
+					Expect(k1.DeleteNamespaceNoWait(controlPlaneNamespace)).To(Succeed(), "Namespace failed to be deleted on Cluster #1")
+					Expect(k2.DeleteNamespaceNoWait(controlPlaneNamespace)).To(Succeed(), "Namespace failed to be deleted on Cluster #2")
+					Expect(k1.DeleteNamespaceNoWait("sample")).To(Succeed(), "Namespace failed to be deleted on Cluster #1")
+					Expect(k2.DeleteNamespaceNoWait("sample")).To(Succeed(), "Namespace failed to be deleted on Cluster #2")
 
-					common.CheckNamespaceEmpty(ctx, clPrimary, controlPlaneNamespace)
-					common.CheckNamespaceEmpty(ctx, clRemote, controlPlaneNamespace)
+					Expect(k1.WaitNamespaceDeleted(controlPlaneNamespace)).To(Succeed())
+					Expect(k2.WaitNamespaceDeleted(controlPlaneNamespace)).To(Succeed())
 					Success("ControlPlane Namespaces are empty")
 
-					// Delete the entire sample namespace in both clusters
-					Expect(k1.DeleteNamespace("sample")).To(Succeed(), "Namespace failed to be deleted on Cluster #1")
-					Expect(k2.DeleteNamespace("sample")).To(Succeed(), "Namespace failed to be deleted on Cluster #2")
-
-					common.CheckNamespaceEmpty(ctx, clPrimary, "sample")
-					common.CheckNamespaceEmpty(ctx, clRemote, "sample")
+					Expect(k1.WaitNamespaceDeleted("sample")).To(Succeed())
+					Expect(k2.WaitNamespaceDeleted("sample")).To(Succeed())
 					Success("Sample app is deleted in both clusters")
 				})
 			})
@@ -276,12 +274,10 @@ spec:
 
 	AfterAll(func(ctx SpecContext) {
 		// Delete the Sail Operator from both clusters
-		Expect(k1.DeleteNamespace(namespace)).To(Succeed(), "Namespace failed to be deleted on Cluster #1")
-		Expect(k2.DeleteNamespace(namespace)).To(Succeed(), "Namespace failed to be deleted on Cluster #2")
-
-		// Delete the intermediate CA from both clusters
-		common.CheckNamespaceEmpty(ctx, clPrimary, namespace)
-		common.CheckNamespaceEmpty(ctx, clRemote, namespace)
+		Expect(k1.DeleteNamespaceNoWait(namespace)).To(Succeed(), "Namespace failed to be deleted on Cluster #1")
+		Expect(k2.DeleteNamespaceNoWait(namespace)).To(Succeed(), "Namespace failed to be deleted on Cluster #2")
+		Expect(k1.WaitNamespaceDeleted(namespace)).To(Succeed())
+		Expect(k2.WaitNamespaceDeleted(namespace)).To(Succeed())
 	})
 })
 
