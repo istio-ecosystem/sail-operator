4d469b0bd0b69664194018d3ba096f933e9acee5
Filip Brychta <fbrychta@redhat.com>
2025-02-14 12:36:41 +0100

    Using stable and stable-3.0 OLM channels (#234)

--- Makefile.vendor.mk
+++ Makefile.vendor.mk
@@ -1,7 +1,8 @@
 VERSION = 3.0.0
 OPERATOR_NAME = servicemeshoperator3
 HUB = quay.io/maistra-dev
-CHANNELS = stable
+CHANNELS = "stable,stable-3.0"
+DEFAULT_CHANNEL = stable
 HELM_VALUES_FILE = ossm/values.yaml
 VERSIONS_YAML_FILE ?= ossm/versions.yaml
 GENERATE_RELATED_IMAGES = false
