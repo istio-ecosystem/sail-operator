d955bfb689f75c54525ffb3d4f5a64e4e7330139
Filip Brychta <fbrychta@redhat.com>
2025-02-24 12:29:15 +0100

    Use the same versions as in the upstream

--- Makefile.vendor.mk
+++ Makefile.vendor.mk
@@ -4,5 +4,4 @@ HUB = quay.io/maistra-dev
 CHANNELS = "stable,stable-3.0"
 DEFAULT_CHANNEL = stable
 HELM_VALUES_FILE = ossm/values.yaml
-VERSIONS_YAML_FILE ?= ossm/versions.yaml
 GENERATE_RELATED_IMAGES = false
