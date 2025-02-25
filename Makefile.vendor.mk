ff680267c39ac8c819b7a89b952f80fd0e8b3bf5
Yuanlin Xu <xuyuanlin_00@hotmail.com>
2025-02-25 14:14:00 -0500

    FIPS: Fix missing build tag warnings (#255)
    * FIPS: Fix missing build tag warnings
    
    Signed-off-by: Yuanlin Xu <yuanlin.xu@redhat.com>
    
    * move FIPS target into vendor.mk
    
    Signed-off-by: Yuanlin Xu <yuanlin.xu@redhat.com>
    
    ---------
    
    Signed-off-by: Yuanlin Xu <yuanlin.xu@redhat.com>

--- Makefile.vendor.mk
+++ Makefile.vendor.mk
@@ -5,3 +5,7 @@ CHANNELS = "stable,stable-3.0"
 DEFAULT_CHANNEL = stable
 HELM_VALUES_FILE = ossm/values.yaml
 GENERATE_RELATED_IMAGES = false
+
+.PHONY: build-fips
+build-fips: ## Build sail-operator binary for FIPS mode.
+	GOARCH="$(TARGET_ARCH)" CGO_ENABLED=1 LDFLAGS="$(LD_FLAGS) -tags strictfipsruntime" common/scripts/gobuild.sh $(REPO_ROOT)/out/$(TARGET_OS)_$(TARGET_ARCH)/sail-operator cmd/main.go
