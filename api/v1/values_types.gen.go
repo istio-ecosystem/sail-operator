bef481835b48c8555edbb72ea07fa024d6f57cf4
Daniel Grimm <dgrimm@redhat.com>
2025-03-10 20:51:13 +0100

    api_transformer: remove `unsafe` import
    Signed-off-by: Daniel Grimm <dgrimm@redhat.com>

--- api/v1/values_types.gen.go
+++ api/v1/values_types.gen.go
@@ -22,7 +22,6 @@ import (
 	k8sv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	intstr "k8s.io/apimachinery/pkg/util/intstr"
-	unsafe "unsafe"
 )
 
 // Mode for the ingress controller.
