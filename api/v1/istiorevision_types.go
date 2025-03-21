43389bb8d66d85c0108c7f8575ab82ec52ab9a81
Marko Lukša <mluksa@redhat.com>
2025-03-21 08:39:42 +0100

    Add NAMESPACE column to Sail resource kinds (#721)
    Signed-off-by: Marko Lukša <mluksa@redhat.com>

--- api/v1/istiorevision_types.go
+++ api/v1/istiorevision_types.go
@@ -176,6 +176,7 @@ const (
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:scope=Cluster,shortName=istiorev,categories=istio-io
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Namespace",type="string",JSONPath=".spec.namespace",description="The namespace for the control plane components."
 // +kubebuilder:printcolumn:name="Type",type="string",JSONPath=".spec.type",description="Whether the control plane is installed locally or in a remote cluster."
 // +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].status",description="Whether the control plane installation is ready to handle requests."
 // +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.state",description="The current state of this object."
