c46caf2d3e044bd35f2b58307397fdab2ef47b2a
Nick Fox <nfox@redhat.com>
2025-04-09 10:40:05 -0400

    Update CHANNEL_PREFIX to stable in makefile
    Signed-off-by: Nick Fox <nfox@redhat.com>

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -89,7 +89,7 @@ GINKGO_FLAGS := $(if $(VERBOSE),-v) $(if $(CI),--no-color) $(if $(COVERAGE),-cov
 # To re-generate a bundle for other specific channels without changing the standard setup, you can:
 # - use the CHANNELS as arg of the bundle target (e.g make bundle CHANNELS=candidate,fast,stable)
 # - use environment variables to overwrite this value (e.g export CHANNELS="candidate,fast,stable")
-CHANNEL_PREFIX := dev
+CHANNEL_PREFIX := stable
 
 CHANNELS ?= $(CHANNEL_PREFIX)-$(MINOR_VERSION)
 ifneq ($(origin CHANNELS), undefined)
