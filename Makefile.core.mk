549fe6c54eb3ba9f2728a324634c7fea23f488ab
Daniel Grimm <dgrimm@redhat.com>
2025-05-07 21:40:26 +0200

    bump operator version to 1.25.2
    Signed-off-by: Daniel Grimm <dgrimm@redhat.com>

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -19,7 +19,7 @@ OLD_VARS := $(.VARIABLES)
 # Use `make print-variables` to inspect the values of the variables
 -include Makefile.vendor.mk
 
-VERSION ?= 1.25.1
+VERSION ?= 1.25.2
 MINOR_VERSION := $(shell echo "${VERSION}" | cut -f1,2 -d'.')
 
 OPERATOR_NAME ?= sailoperator
