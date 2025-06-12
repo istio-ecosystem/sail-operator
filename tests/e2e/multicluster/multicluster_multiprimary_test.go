5abcf84aad31cee67e3d0f7f5323e801b1114c1b
Mike Kolesnik <mkolesni@redhat.com>
2025-06-12 16:45:26 +0300

    Consolidate multicluster operator deploy/cleanup (#902)
    Now that we have `Cleaner` clean up after each test, it makes sense to
    run this logic once in the suite, rather than after each test (which
    cleans up its own resources).
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- tests/e2e/multicluster/multicluster_multiprimary_test.go
+++ tests/e2e/multicluster/multicluster_multiprimary_test.go
@@ -19,19 +19,16 @@ package multicluster
 import (
 	"context"
 	"fmt"
-	"path/filepath"
 	"time"
 
 	v1 "github.com/istio-ecosystem/sail-operator/api/v1"
 	"github.com/istio-ecosystem/sail-operator/pkg/istioversion"
 	"github.com/istio-ecosystem/sail-operator/pkg/kube"
-	"github.com/istio-ecosystem/sail-operator/pkg/test/project"
 	. "github.com/istio-ecosystem/sail-operator/pkg/test/util/ginkgo"
 	"github.com/istio-ecosystem/sail-operator/tests/e2e/util/certs"
 	"github.com/istio-ecosystem/sail-operator/tests/e2e/util/cleaner"
 	"github.com/istio-ecosystem/sail-operator/tests/e2e/util/common"
 	. "github.com/istio-ecosystem/sail-operator/tests/e2e/util/gomega"
-	"github.com/istio-ecosystem/sail-operator/tests/e2e/util/helm"
 	"github.com/istio-ecosystem/sail-operator/tests/e2e/util/istioctl"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -44,36 +41,6 @@ import (
 var _ = Describe("Multicluster deployment models", Label("multicluster", "multicluster-multiprimary"), Ordered, func() {
 	SetDefaultEventuallyTimeout(180 * time.Second)
 	SetDefaultEventuallyPollingInterval(time.Second)
-	debugInfoLogged := false
-	clr1 := cleaner.New(clPrimary, "cluster=primary")
-	clr2 := cleaner.New(clRemote, "cluster=remote")
-
-	BeforeAll(func(ctx SpecContext) {
-		clr1.Record(ctx)
-		clr2.Record(ctx)
-
-		if !skipDeploy {
-			// Deploy the Sail Operator on both clusters
-			Expect(k1.CreateNamespace(namespace)).To(Succeed(), "Namespace failed to be created on Cluster #1")
-			Expect(k2.CreateNamespace(namespace)).To(Succeed(), "Namespace failed to be created on Cluster #2")
-
-			Expect(helm.Install("sail-operator", filepath.Join(project.RootDir, "chart"), "--namespace "+namespace, "--set=image="+image, "--kubeconfig "+kubeconfig)).
-				To(Succeed(), "Operator failed to be deployed in Cluster #1")
-
-			Expect(helm.Install("sail-operator", filepath.Join(project.RootDir, "chart"), "--namespace "+namespace, "--set=image="+image, "--kubeconfig "+kubeconfig2)).
-				To(Succeed(), "Operator failed to be deployed in Cluster #2")
-
-			Eventually(common.GetObject).
-				WithArguments(ctx, clPrimary, kube.Key(deploymentName, namespace), &appsv1.Deployment{}).
-				Should(HaveConditionStatus(appsv1.DeploymentAvailable, metav1.ConditionTrue), "Error getting Istio CRD")
-			Success("Operator is deployed in the Cluster #1 namespace and Running")
-
-			Eventually(common.GetObject).
-				WithArguments(ctx, clRemote, kube.Key(deploymentName, namespace), &appsv1.Deployment{}).
-				Should(HaveConditionStatus(appsv1.DeploymentAvailable, metav1.ConditionTrue), "Error getting Istio CRD")
-			Success("Operator is deployed in the Cluster #2 namespace and Running")
-		}
-	})
 
 	Describe("Multi-Primary Multi-Network configuration", func() {
 		// Test the Multi-Primary Multi-Network configuration for each supported Istio version
@@ -363,23 +330,5 @@ spec:
 				})
 			})
 		}
-	})
-
-	AfterAll(func(ctx SpecContext) {
-		if CurrentSpecReport().Failed() {
-			if !debugInfoLogged {
-				common.LogDebugInfo(common.MultiCluster, k1, k2)
-				debugInfoLogged = true
-			}
-
-			if keepOnFailure {
-				return
-			}
-		}
-
-		c1Deleted := clr1.CleanupNoWait(ctx)
-		c2Deleted := clr2.CleanupNoWait(ctx)
-		clr1.WaitForDeletion(ctx, c1Deleted)
-		clr2.WaitForDeletion(ctx, c2Deleted)
 	})
 })
