796d5f7e4d2210b64860edf243c0e31045577f5b
Tianpeng Wang <tpwang@alauda.io>
2025-06-19 14:34:11 +0800

    build: fix release pipeline
    Signed-off-by: Tianpeng Wang <tpwang@alauda.io>

--- Makefile.vendor.mk
+++ Makefile.vendor.mk
@@ -9,3 +9,20 @@ VERSIONS_YAML_FILE ?= alauda-versions.yaml
 GENERATE_RELATED_IMAGES = false
 
 PLATFORMS ?= linux/arm64,linux/amd64
+
+
+ALAUDA_PLATFORM_ARCHITECTURES := amd64 arm64
+ALAUDA_BUILD_TARGETS := $(addprefix alauda-build-linux-,$(ALAUDA_PLATFORM_ARCHITECTURES))
+
+.PHONY: alauda-build-all
+alauda-build-all: $(ALAUDA_BUILD_TARGETS)
+
+alauda-build-linux-%:
+	@echo "Building for architecture: $*"
+	GOOS=linux GOARCH=$* CGO_ENABLED=$(CGO_ENABLED) LDFLAGS="$(LD_FLAGS)" \
+		common/scripts/gobuild.sh $(REPO_ROOT)/out/linux_$*/sail-operator cmd/main.go
+
+.PHONY: alauda-docker-buildx
+alauda-docker-buildx: alauda-build-all
+	docker buildx build $(BUILDX_OUTPUT) --platform=$(PLATFORMS) -f Dockerfile.alauda --tag ${IMAGE} \
+	$(BUILDX_ADDITIONAL_TAGS) .
