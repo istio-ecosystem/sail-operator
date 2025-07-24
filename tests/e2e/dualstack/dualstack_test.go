c95850a42b1a3650180c7a94fef210b6f7684518
Francisco H <frherrer@redhat.com>
2025-07-24 16:44:16 +0000

    Fix lint
    Signed-off-by: Francisco H <frherrer@redhat.com>
    Signed-off-by: Francisco Herrera <fjglira@gmail.com>

--- tests/e2e/dualstack/dualstack_test.go
+++ tests/e2e/dualstack/dualstack_test.go
@@ -159,10 +159,18 @@ values:
 						Expect(k.Label("namespace", IPv6Namespace, "istio-injection", "enabled")).To(Succeed(), "Error labeling ipv6 namespace")
 						Expect(k.Label("namespace", SleepNamespace, "istio-injection", "enabled")).To(Succeed(), "Error labeling sleep namespace")
 
-						Expect(k.WithNamespace(DualStackNamespace).ApplyKustomize(common.GetKustomizeDir("tcp-echo-dual-stack"))).To(Succeed(), "error deploying tcpDualStack pod")
-						Expect(k.WithNamespace(IPv4Namespace).ApplyKustomize(common.GetKustomizeDir("tcp-echo-ipv4"))).To(Succeed(), "error deploying ipv4 pod")
-						Expect(k.WithNamespace(IPv6Namespace).ApplyKustomize(common.GetKustomizeDir("tcp-echo-ipv6"))).To(Succeed(), "error deploying ipv6 pod")
-						Expect(k.WithNamespace(SleepNamespace).ApplyKustomize(common.GetKustomizeDir("sleep"))).To(Succeed(), "error deploying sleep pod")
+						Expect(k.WithNamespace(DualStackNamespace).
+							ApplyKustomize(common.GetKustomizeDir("tcp-echo-dual-stack"))).
+							To(Succeed(), "error deploying tcpDualStack pod")
+						Expect(k.WithNamespace(IPv4Namespace).
+							ApplyKustomize(common.GetKustomizeDir("tcp-echo-ipv4"))).
+							To(Succeed(), "error deploying ipv4 pod")
+						Expect(k.WithNamespace(IPv6Namespace).
+							ApplyKustomize(common.GetKustomizeDir("tcp-echo-ipv6"))).
+							To(Succeed(), "error deploying ipv6 pod")
+						Expect(k.WithNamespace(SleepNamespace).
+							ApplyKustomize(common.GetKustomizeDir("sleep"))).
+							To(Succeed(), "error deploying sleep pod")
 
 						Success("dualStack validation pods deployed")
 					})
