054829dd4bc0d704aff11acc95253aa704dc74a8
Francisco Herrera <fjglira@gmail.com>
2025-07-29 17:53:49 +0200

    Handle SELinux issues while running using podman
    Signed-off-by: Francisco Herrera <fjglira@gmail.com>

--- Makefile.overrides.mk
+++ Makefile.overrides.mk
@@ -20,6 +20,10 @@ CONTAINER_OPTIONS = --mount type=bind,source=/tmp,destination=/tmp --net=host
 
 export COMMONFILES_POSTPROCESS = tools/commonfiles-postprocess.sh
 
+ifeq ($(CONTAINER_CLI),podman)
+CONTAINER_OPTIONS += --security-opt label=disable
+endif
+
 ifeq ($(BUILD_WITH_CONTAINER),1)
 # create phony targets for the top-level items in the repo
 PHONYS := $(shell ls | grep -v Makefile)
