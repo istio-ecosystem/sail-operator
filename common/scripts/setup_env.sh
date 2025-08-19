14a2e6199244775abcd854e05399531d00388330
Francisco Herrera <fjglira@gmail.com>
2025-08-19 09:43:12 +0200

    Update Channels, Tags and charts for release-1.27
    Signed-off-by: Francisco Herrera <fjglira@gmail.com>

--- common/scripts/setup_env.sh
+++ common/scripts/setup_env.sh
@@ -75,7 +75,7 @@ fi
 TOOLS_REGISTRY_PROVIDER=${TOOLS_REGISTRY_PROVIDER:-gcr.io}
 PROJECT_ID=${PROJECT_ID:-istio-testing}
 if [[ "${IMAGE_VERSION:-}" == "" ]]; then
-  IMAGE_VERSION=master-672e6089ff843019a2b28cf9e87754c7b74358ea
+  IMAGE_VERSION=release-1.27-56a42ed008b9070d88166dbec82e798d9eca43de
 fi
 if [[ "${IMAGE_NAME:-}" == "" ]]; then
   IMAGE_NAME=build-tools
