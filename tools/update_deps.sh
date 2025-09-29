d851924f32b58aedb9e5816a9293a1d3dff055bc
Francisco Herrera <fjglira@gmail.com>
2025-09-29 18:01:52 +0200

    Improve by using gsed for macOS and sed for Linux
    Improves from code review
    
    Signed-off-by: Francisco Herrera <fjglira@gmail.com>

--- tools/update_deps.sh
+++ tools/update_deps.sh
@@ -17,11 +17,15 @@
 set -exo pipefail
 
 # Set up a cross-platform sed command.
-# On macOS (Darwin), sed -i requires an empty string argument. On Linux, it does not.
-sedi_args=("-i")
+# On macOS, we use gsed (GNU sed) to have consistent behavior with Linux.
+# This requires gsed to be installed on macOS (e.g., via `brew install gnu-sed`).
+sed_executable="sed"
 if [[ "$(uname)" == "Darwin" ]]; then
-  sedi_args=("-i" "")
+  sed_executable="gsed"
 fi
+# Create a command array with the in-place flag.
+SED_CMD=("$sed_executable" "-i")
+
 
 UPDATE_BRANCH=${UPDATE_BRANCH:-"master"}
 
@@ -50,7 +54,7 @@ make update-common
 
 # update build container used in github actions
 NEW_IMAGE_MASTER=$(grep IMAGE_VERSION= < common/scripts/setup_env.sh | cut -d= -f2)
-sed "${sedi_args[@]}" -e "s|\(gcr.io/istio-testing/build-tools\):master.*|\1:$NEW_IMAGE_MASTER|" .github/workflows/update-deps.yaml
+"${SED_CMD[@]}" -e "s|\(gcr.io/istio-testing/build-tools\):master.*|\1:$NEW_IMAGE_MASTER|" .github/workflows/update-deps.yaml
 
 # Update go dependencies
 export GO111MODULE=on
@@ -60,16 +64,16 @@ go mod tidy
 
 # Update operator-sdk
 OPERATOR_SDK_LATEST_VERSION=$(getLatestVersion operator-framework/operator-sdk)
-sed "${sedi_args[@]}" "s|OPERATOR_SDK_VERSION ?= .*|OPERATOR_SDK_VERSION ?= ${OPERATOR_SDK_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
-find "${ROOTDIR}/chart/templates/olm/scorecard.yaml" -type f -exec sed "${sedi_args[@]}" "s|quay.io/operator-framework/scorecard-test:.*|quay.io/operator-framework/scorecard-test:${OPERATOR_SDK_LATEST_VERSION}|" {} +
+"${SED_CMD[@]}" "s|OPERATOR_SDK_VERSION ?= .*|OPERATOR_SDK_VERSION ?= ${OPERATOR_SDK_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+find "${ROOTDIR}/chart/templates/olm/scorecard.yaml" -type f -exec "${SED_CMD[@]}" "s|quay.io/operator-framework/scorecard-test:.*|quay.io/operator-framework/scorecard-test:${OPERATOR_SDK_LATEST_VERSION}|" {} +
 
 # Update helm
 HELM_LATEST_VERSION=$(getLatestVersion helm/helm | cut -d/ -f2)
-sed "${sedi_args[@]}" "s|HELM_VERSION ?= .*|HELM_VERSION ?= ${HELM_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+"${SED_CMD[@]}" "s|HELM_VERSION ?= .*|HELM_VERSION ?= ${HELM_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
 
 # Update controller-tools
 CONTROLLER_TOOLS_LATEST_VERSION=$(getLatestVersion kubernetes-sigs/controller-tools)
-sed "${sedi_args[@]}" "s|CONTROLLER_TOOLS_VERSION ?= .*|CONTROLLER_TOOLS_VERSION ?= ${CONTROLLER_TOOLS_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+"${SED_CMD[@]}" "s|CONTROLLER_TOOLS_VERSION ?= .*|CONTROLLER_TOOLS_VERSION ?= ${CONTROLLER_TOOLS_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
 
 # Update controller-runtime
 CONTROLLER_RUNTIME_LATEST_VERSION=$(getLatestVersion kubernetes-sigs/controller-runtime)
@@ -77,41 +81,41 @@ CONTROLLER_RUNTIME_LATEST_VERSION=$(getLatestVersion kubernetes-sigs/controller-
 # go get -u "sigs.k8s.io/controller-runtime@${CONTROLLER_RUNTIME_LATEST_VERSION}"
 go get "sigs.k8s.io/controller-runtime@${CONTROLLER_RUNTIME_LATEST_VERSION}"
 CONTROLLER_RUNTIME_BRANCH=$(getReleaseBranch "${CONTROLLER_RUNTIME_LATEST_VERSION}")
-sed "${sedi_args[@]}" "s|CONTROLLER_RUNTIME_BRANCH ?= .*|CONTROLLER_RUNTIME_BRANCH ?= ${CONTROLLER_RUNTIME_BRANCH}|" "${ROOTDIR}/Makefile.core.mk"
+"${SED_CMD[@]}" "s|CONTROLLER_RUNTIME_BRANCH ?= .*|CONTROLLER_RUNTIME_BRANCH ?= ${CONTROLLER_RUNTIME_BRANCH}|" "${ROOTDIR}/Makefile.core.mk"
 
 # Update opm
 OPM_LATEST_VERSION=$(getLatestVersion operator-framework/operator-registry)
-sed "${sedi_args[@]}" "s|OPM_VERSION ?= .*|OPM_VERSION ?= ${OPM_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+"${SED_CMD[@]}" "s|OPM_VERSION ?= .*|OPM_VERSION ?= ${OPM_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
 
 # Update olm
 OLM_LATEST_VERSION=$(getLatestVersion operator-framework/operator-lifecycle-manager)
-sed "${sedi_args[@]}" "s|OLM_VERSION ?= .*|OLM_VERSION ?= ${OLM_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+"${SED_CMD[@]}" "s|OLM_VERSION ?= .*|OLM_VERSION ?= ${OLM_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
 
 # Update kube-rbac-proxy
 RBAC_PROXY_LATEST_VERSION=$(getLatestVersion brancz/kube-rbac-proxy | cut -d/ -f1)
 # Only update it if the newer image is available in the registry
 if docker manifest inspect "gcr.io/kubebuilder/kube-rbac-proxy:${RBAC_PROXY_LATEST_VERSION}" >/dev/null 2>/dev/null; then
-  sed "${sedi_args[@]}" "s|gcr.io/kubebuilder/kube-rbac-proxy:.*|gcr.io/kubebuilder/kube-rbac-proxy:${RBAC_PROXY_LATEST_VERSION}|" "${ROOTDIR}/chart/values.yaml"
+  "${SED_CMD[@]}" "s|gcr.io/kubebuilder/kube-rbac-proxy:.*|gcr.io/kubebuilder/kube-rbac-proxy:${RBAC_PROXY_LATEST_VERSION}|" "${ROOTDIR}/chart/values.yaml"
 fi
 
 # Update gitleaks
 GITLEAKS_VERSION=$(getLatestVersion gitleaks/gitleaks)
-sed "${sedi_args[@]}" "s|GITLEAKS_VERSION ?= .*|GITLEAKS_VERSION ?= ${GITLEAKS_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+"${SED_CMD[@]}" "s|GITLEAKS_VERSION ?= .*|GITLEAKS_VERSION ?= ${GITLEAKS_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
 
 # Update runme
 RUNME_LATEST_VERSION=$(getLatestVersion runmedev/runme)
 # Remove the leading "v" from the version string
 RUNME_LATEST_VERSION=${RUNME_LATEST_VERSION#v}
-sed "${sedi_args[@]}" "s|RUNME_VERSION ?= .*|RUNME_VERSION ?= ${RUNME_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+"${SED_CMD[@]}" "s|RUNME_VERSION ?= .*|RUNME_VERSION ?= ${RUNME_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
 
 # Update misspell
 MISSPELL_LATEST_VERSION=$(getLatestVersion client9/misspell)
-sed "${sedi_args[@]}" "s|MISSPELL_VERSION ?= .*|MISSPELL_VERSION ?= ${MISSPELL_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+"${SED_CMD[@]}" "s|MISSPELL_VERSION ?= .*|MISSPELL_VERSION ?= ${MISSPELL_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
 
 # Update KIND_IMAGE. Look for KIND_IMAGE := docker.io in the make file and look on docker.io/kindest/node for the latest version.
 KIND_LATEST_VERSION=$(getLatestVersionFromDockerHub kindest/node)
 if [[ -n "${KIND_LATEST_VERSION}" ]]; then
-  sed "${sedi_args[@]}" "s|KIND_IMAGE := docker.io/kindest/node:.*|KIND_IMAGE := docker.io/kindest/node:${KIND_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+  "${SED_CMD[@]}" "s|KIND_IMAGE := docker.io/kindest/node:.*|KIND_IMAGE := docker.io/kindest/node:${KIND_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
 else
   echo "No latest version found for kindest/node on Docker Hub. Keeping the existing KIND_IMAGE."
 fi
