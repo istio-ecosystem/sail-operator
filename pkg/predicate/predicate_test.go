e169df4b192fd4ae8f8f23e8d57288d52b115136
bmangoen <bmangoen@redhat.com>
2025-10-06 15:22:15 +0200

    Watch all resources with IgnoreUpdateWhenAnnotation predicate
    Signed-off-by: bmangoen <bmangoen@redhat.com>

--- pkg/predicate/predicate_test.go
+++ pkg/predicate/predicate_test.go
@@ -18,12 +18,36 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 )
 
 func TestIgnoreUpdate(t *testing.T) {
 	predicate := IgnoreUpdate()
 	assert.Equal(t, false, predicate.Update(event.UpdateEvent{}))
+	assert.Equal(t, true, predicate.Create(event.CreateEvent{}))
+	assert.Equal(t, true, predicate.Delete(event.DeleteEvent{}))
+	assert.Equal(t, true, predicate.Generic(event.GenericEvent{}))
+}
+
+func TestIgnoreUpdateWhenAnnotation(t *testing.T) {
+	predicate := IgnoreUpdateWhenAnnotation()
+	assert.Equal(t, false, predicate.Update(event.UpdateEvent{
+		ObjectOld: &corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{},
+		},
+		ObjectNew: &corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{
+					"sailoperator.io/ignore": "true",
+				},
+			},
+			Data: map[string]string{
+				"foo": "bar",
+			},
+		},
+	}))
 	assert.Equal(t, true, predicate.Create(event.CreateEvent{}))
 	assert.Equal(t, true, predicate.Delete(event.DeleteEvent{}))
 	assert.Equal(t, true, predicate.Generic(event.GenericEvent{}))
