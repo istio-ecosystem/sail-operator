a9be24b3d9736147b504b48df91d1c8e389c3a41
Mike Kolesnik <mkolesni@redhat.com>
2025-11-20 17:41:03 +0200

    Make sure to skip cleanup of operator in e2e (#1351)
    In case it was requested, don't delete the deployed operator.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>
    Signed-off-by: Rafael Zago <rafaelvzago@gmail.com>

--- tests/e2e/common-operator-integ-suite.sh
+++ tests/e2e/common-operator-integ-suite.sh
@@ -195,7 +195,7 @@ uninstall_operator() {
 cleanup() {
   # Do not let cleanup errors affect the final exit code
   set +e
-  if [ "${OLM}" != "true" ] && [ "${SKIP_DEPLOY}" != "true" ]; then
+  if [ "${OLM}" != "true" ] && [ "${SKIP_DEPLOY}" != "true" ] && [ "${SKIP_CLEANUP}" != "true" ]; then
     if [ "${MULTICLUSTER}" == true ]; then
       KUBECONFIG="${KUBECONFIG}" uninstall_operator || true
       # shellcheck disable=SC2153  # KUBECONFIG2 is set by multicluster setup scripts
