8e2cecfc6724862dd6ba746e7674c86bd6be598b
Mike Kolesnik <mkolesni@redhat.com>
2025-11-25 11:22:23 +0200

    E2E: Add check that multicluster gateway rechable
    To rule out infrastructure problems, a sanity check is added to try and
    reach the gateway on the other side.
    If this check fails, this could indicate an infrastructure problem.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- tests/e2e/multicluster/common.go
+++ tests/e2e/multicluster/common.go
@@ -165,3 +165,13 @@ func expectLoadBalancerAddress(ctx context.Context, k kubectl.Kubectl, cl client
 	Expect(address).ToNot(BeEmpty(), fmt.Sprintf("Gateway service %q does not have an external address on %s", name, k.ClusterName))
 	Success(fmt.Sprintf("Gateway service %q has an external address %s on %s", name, address, k.ClusterName))
 }
+
+// eventuallyLoadBalancerIsReachable to make sure that cross cluster communication is working on the infrastructure level
+func eventuallyLoadBalancerIsReachable(ctx context.Context, kSrc kubectl.Kubectl, kDest kubectl.Kubectl, clDest client.Client, name string) {
+	address := common.GetSVCLoadBalancerAddress(ctx, clDest, controlPlaneNamespace, name)
+	Eventually(ctx, kSrc.WithNamespace("sample").Exec).WithArguments(
+		"deploy/sleep", "sleep", fmt.Sprintf("curl -sS -o /dev/null -w '%%{http_code}' %s:15021/healthz/ready", address)).
+		Should(Equal("200"), fmt.Sprintf("Gateway %q(%s) on %s is not reachable from %s, this might indicate a problem with the underlying infrastructure",
+			name, address, kDest.ClusterName, kSrc.ClusterName))
+	Success(fmt.Sprintf("Gateway %q(%s) on %s is reachable from %s", name, address, kDest.ClusterName, kSrc.ClusterName))
+}
