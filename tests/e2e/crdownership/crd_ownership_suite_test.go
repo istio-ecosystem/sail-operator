4eed2f7578e7614d7e377920bd28be1f05ae5e7c
Daniel Grimm <dgrimm@redhat.com>
2026-04-17 11:25:59 +0200

    pkg/install library
    This is a rewrite of @aslak's install library effort
    (https://github.com/openshift-service-mesh/sail-operator/pull/721).
    
    Notable improvements:
    - watches are the same as for the regular controllers
    - required rbac is derived from watches
    - typed informers/clients everywhere
    - reuses ComputeValues/Reconciler logic whereever possible
    - image digests are no longer pulled from CSV YAML but are generated and
      stored in pkg/install/images.gen.go
    - multiple e2e tests
    
    Signed-off-by: Daniel Grimm <dgrimm@redhat.com>

---
+++ tests/e2e/crdownership/crd_ownership_suite_test.go
@@ -0,0 +1,77 @@
+//go:build e2e
+
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package crdownership
+
+import (
+	"os"
+	"testing"
+
+	"github.com/istio-ecosystem/sail-operator/pkg/env"
+	k8sclient "github.com/istio-ecosystem/sail-operator/tests/e2e/util/client"
+	"github.com/istio-ecosystem/sail-operator/tests/e2e/util/kubectl"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+var (
+	cl            client.Client
+	kubeConfig    *rest.Config
+	dynamicClient dynamic.Interface
+	k             kubectl.Kubectl
+	multicluster  = env.GetBool("MULTICLUSTER", false)
+)
+
+const (
+	libraryNamespace = "crd-ownership-test"
+)
+
+func TestCRDOwnership(t *testing.T) {
+	if multicluster {
+		t.Skip("Skipping the CRD Ownership tests")
+	}
+
+	RegisterFailHandler(Fail)
+	setup()
+	RunSpecs(t, "CRD Ownership Test Suite")
+}
+
+func setup() {
+	GinkgoWriter.Println("************ Running Setup ************")
+	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+
+	var err error
+
+	GinkgoWriter.Println("Initializing k8s client")
+	cl, err = k8sclient.InitK8sClient("")
+	Expect(err).NotTo(HaveOccurred())
+
+	GinkgoWriter.Println("Building kubeconfig")
+	kubeConfig, err = clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
+	Expect(err).NotTo(HaveOccurred())
+
+	GinkgoWriter.Println("Creating dynamic client")
+	dynamicClient, err = dynamic.NewForConfig(kubeConfig)
+	Expect(err).NotTo(HaveOccurred())
+
+	k = kubectl.New()
+}
