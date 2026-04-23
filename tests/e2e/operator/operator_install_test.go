d82d87d040e590ebed48ce6157cd736756e903a6
Francisco Herrera <fjglira@gmail.com>
2026-04-23 15:44:17 +0200

    tests: Skip TLS profile change test when is executed on Hosted clusters (#1873)
    * tests: Skip TLS profile change test when is executed on Hosted clusters
    
    There are failures when the test is executed on hosted cluster because by default changing the TLS profile
    
    Signed-off-by: Francisco Herrera <fjglira@gmail.com>
    
    * Adding comment
    
    Adding comment to let folks to know the reason of the skip
    
    Signed-off-by: Francisco Herrera <fjglira@gmail.com>
    
    ---------
    
    Signed-off-by: Francisco Herrera <fjglira@gmail.com>

--- tests/e2e/operator/operator_install_test.go
+++ tests/e2e/operator/operator_install_test.go
@@ -230,6 +230,16 @@ spec:
 				Skip("Skipping OpenShift-specific tests on non-OpenShift cluster")
 			}
 
+			// On hosted clusters, the APIServer resource is read-only and TLS settings cannot be changed,
+			// so skip all TLS profile tests. If the test run in a regular cluster, the APIServer resource is writable and the tests can run as normal.
+			Step("Checking if this is a Hosted Cluster")
+			infra := &configv1.Infrastructure{}
+			infraErr := cl.Get(ctx, client.ObjectKey{Name: "cluster"}, infra)
+			Expect(infraErr).NotTo(HaveOccurred(), "Failed to get Infrastructure resource")
+			if infra.Status.ControlPlaneTopology == configv1.ExternalTopologyMode {
+				Skip("Skipping TLS profile tests on hosted cluster: APIServer resource is read-only on hosted clusters")
+			}
+
 			Step("Determining OpenShift version")
 			cv := &configv1.ClusterVersion{}
 			err := cl.Get(ctx, client.ObjectKey{Name: "version"}, cv)
