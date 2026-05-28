42511f027ffd2f3c98601feb7149bec852a120f0
Mayleigh Tjapkes <mamyers@redhat.com>
2026-05-28 12:56:28 -0400

    Add monitoring.enabled field to Istio CR for COO integration

--- pkg/scheme/scheme.go
+++ pkg/scheme/scheme.go
@@ -19,17 +19,12 @@ import (
 	"github.com/istio-ecosystem/sail-operator/api/v1alpha1"
 	multusv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
 	configv1 "github.com/openshift/api/config/v1"
-	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 
 	networkingv1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
 )
-
-// rhobsAPIGroup is the API group for Cluster Observability Operator (COO) monitoring resources
-const rhobsAPIGroup = "monitoring.rhobs"
 
 var Scheme = runtime.NewScheme()
 
@@ -42,29 +37,5 @@ func init() {
 	utilruntime.Must(v1alpha1.AddToScheme(Scheme))
 	utilruntime.Must(v1.AddToScheme(Scheme))
 
-	// Register prometheus-operator monitoring types with the rhobs API group
-	// This allows us to use typed Go objects while targeting the monitoring.rhobs/v1 API
-	addRhobsMonitoringTypes(Scheme)
-
 	// +kubebuilder:scaffold:scheme
-}
-
-// addRhobsMonitoringTypes registers prometheus-operator monitoring types with the monitoring.rhobs API group
-func addRhobsMonitoringTypes(scheme *runtime.Scheme) {
-	scheme.AddKnownTypeWithName(
-		schema.GroupVersionKind{Group: rhobsAPIGroup, Version: "v1", Kind: "ServiceMonitor"},
-		&monitoringv1.ServiceMonitor{},
-	)
-	scheme.AddKnownTypeWithName(
-		schema.GroupVersionKind{Group: rhobsAPIGroup, Version: "v1", Kind: "ServiceMonitorList"},
-		&monitoringv1.ServiceMonitorList{},
-	)
-	scheme.AddKnownTypeWithName(
-		schema.GroupVersionKind{Group: rhobsAPIGroup, Version: "v1", Kind: "PodMonitor"},
-		&monitoringv1.PodMonitor{},
-	)
-	scheme.AddKnownTypeWithName(
-		schema.GroupVersionKind{Group: rhobsAPIGroup, Version: "v1", Kind: "PodMonitorList"},
-		&monitoringv1.PodMonitorList{},
-	)
 }
