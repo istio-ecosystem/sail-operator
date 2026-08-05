c41b3a9c574f3b99c12a48cf27b9d4fbde275e74
Matej Kralik <mkralik@redhat.com>
2026-08-05 10:43:01 +0200

    Update bundle version to 1.31 (#2209)
    Updated bundle version to 1.31 since contains 1.31 alpha
    
    Signed-off-by: mkralik3 <mkralik@redhat.com>

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -19,7 +19,7 @@ OLD_VARS := $(.VARIABLES)
 # Use `make print-variables` to inspect the values of the variables
 -include Makefile.vendor.mk
 
-VERSION ?= 1.30.0
+VERSION ?= 1.31.0
 MINOR_VERSION := $(shell echo "${VERSION}" | cut -f1,2 -d'.')
 
 # PREVIOUS_VERSION is used to generate the OLM upgrade graph in FBC for stable releases.
