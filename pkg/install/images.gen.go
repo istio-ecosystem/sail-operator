3cf6c2227c1c0e47ad1650e9eb5ebc5330e05efe
Filip Brychta <fbrychta@redhat.com>
2026-09-17 20:55:33 +0200

    Adding Istio 1.31.0 and removing 1.31.0-beta.1 (#2343)
    * Adding Istio 1.30.0 and removing 1.31.0-beta.1
    
    Adding a new istio release
    
    Signed-off-by: Filip Brychta <fbrychta@redhat.com>
    
    * Also update docs
    
    Signed-off-by: Filip Brychta <fbrychta@redhat.com>
    
    ---------
    
    Signed-off-by: Filip Brychta <fbrychta@redhat.com>

--- pkg/install/images.gen.go
+++ pkg/install/images.gen.go
@@ -20,11 +20,11 @@ import "github.com/istio-ecosystem/sail-operator/pkg/config"
 
 func init() {
 	config.Config.ImageDigests = map[string]config.IstioImageConfig{
-		"v1.31.0-beta.1": {
-			IstiodImage:  "quay.io/sail-dev/pilot:1.31.0-beta.1",
-			ProxyImage:   "quay.io/sail-dev/proxyv2:1.31.0-beta.1",
-			CNIImage:     "quay.io/sail-dev/install-cni:1.31.0-beta.1",
-			ZTunnelImage: "quay.io/sail-dev/ztunnel:1.31.0-beta.1",
+		"v1.31.0": {
+			IstiodImage:  "quay.io/sail-dev/pilot:1.31.0",
+			ProxyImage:   "quay.io/sail-dev/proxyv2:1.31.0",
+			CNIImage:     "quay.io/sail-dev/install-cni:1.31.0",
+			ZTunnelImage: "quay.io/sail-dev/ztunnel:1.31.0",
 		},
 		"v1.30.4": {
 			IstiodImage:  "registry.istio.io/release/pilot:1.30.4",
